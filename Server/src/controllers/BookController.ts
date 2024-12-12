6ac1a80a235c747b7a63c57db44c80122ca73317
Mohammed Aziz Hammemi <Elhammemi001@gmail.com>
2024-12-12 16:44:09 +0100

    done the query book function in the back-end

--- Server/src/controllers/BookController.ts
+++ Server/src/controllers/BookController.ts
@@ -1,5 +1,5 @@
 import { Request,Response } from "express";
-import { findAllBooks,registerBook,modifyBook,removeBook } from "../services/BookService";
+import { findAllBooks,registerBook,modifyBook,removeBook, queryBooks } from "../services/BookService";
 
 import { IBook } from "../models/Book";
 import { IBookModel } from "../daos/BookDao";
@@ -52,4 +52,22 @@ async function deleteBook(req: Request, res: Response){
 }
 }
 
-export default {getAllBooks,createBook,updateBook,deleteBook};
+async function searchForBooksByQuery(req:Request, res:Response){
+    let {title,barcode,description,author,subject,genre,page = 1,limit = 25} = req.query;
+    try{
+        let books = await queryBooks(
+            Number(page),
+            Number(limit),
+            title as string,
+            barcode as string,
+            description as string,
+            author as string,
+            subject as string,
+            genre as string);
+        res.status(200).json({message: "Books retrieved successfully",page : books});
+    }catch(error:any){
+        res.status(500).json({message: "Unable to retrieve books at this time",error});
+    }
+}
+
+export default {getAllBooks,createBook,updateBook,deleteBook,searchForBooksByQuery};

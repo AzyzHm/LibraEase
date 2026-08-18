80b5cb7cc76176896dd35f1967a2e4dfdd6f4bce
Mohammed Aziz Hammemi <Elhammemi001@gmail.com>
2026-08-18 07:36:43 +0100

    Remove unused files and dependencies from the library-app project

--- App/src/features/navigation/index.tsx
+++
@@ -1,4 +0,0 @@
-import { Navbar } from "./components/Navbar/Navbar";
-import { Footer } from "./components/Footer/Footer";
-
-export { Navbar, Footer };
--- App/src/features/profile/index.ts
+++
@@ -1,4 +0,0 @@
-import { UpdateUserForm } from "./components/UpdateUserForm/UpdateUserForm";
-import { ProfileLoanHistory } from "./components/ProfileLoanHistory/ProfileLoanHistory";
-
-export { UpdateUserForm, ProfileLoanHistory };

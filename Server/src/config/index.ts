cf0c452be7b6bc7e2bc86e20f13cb03444513538
Mohammed Aziz Hammemi <Elhammemi001@gmail.com>
2026-08-25 13:47:13 +0100

    fix: standardize environment variable names for server configuration

--- Server/src/config/index.ts
+++ Server/src/config/index.ts
@@ -5,9 +5,9 @@ dotenv.config();
 const supabase_url: string = process.env.SUPABASE_URL || '';
 const supabase_service_key: string = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
 
-const PORT: number = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 8000;
-const ROUNDS: number = process.env.SERVER_ROUNDS
-  ? parseInt(process.env.SERVER_ROUNDS)
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const ROUNDS: number = process.env.ROUNDS
+  ? parseInt(process.env.ROUNDS)
   : Math.floor(Math.random() * 10) + 1;
 
 const jwtSecret: string =
